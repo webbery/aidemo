b780861b0190cbc8d5217f7fc2fdd53390ad3934
webberg <webbery@foxmail.com>
2019-12-10 14:17:10 +0800

    添加每个节点的文本密度作为size

--- algorithm/tools/dom_tree.py
+++ algorithm/tools/dom_tree.py
@@ -2,6 +2,7 @@ from lxml import etree
 # import networkx as nx
 # import matplotlib.pyplot as plt
 import json
+import math
 
 class DomTree():
     def __init__(self):
@@ -37,17 +38,36 @@ class DomTree():
                 # print(text)
                 # node_name += "["+ text+ "]"
             child = {'name':node_name}
+            child['size'] = math.ceil(self.char_density(elm))
             this_children = self.get_children(elm, node_name)
             if len(this_children)!=0:
                 child["children"] = this_children
             children.append(child)
         return children
 
+    # 计算节点的字符密度
     def char_density(self,node):
-        pass
+        # 1. get word count
+        text = node.xpath('string()')
+        word_len = len(str(text))
+        # 2. tag <a> word count
+        a = node.xpath('.//a')
+        a_text = ''
+        for n in a:
+            a_text += n.xpath('string()')
+        a_len = len(a_text)
+        # 3. get all children
+        children = node.xpath('//*')
+        children_count = len(children)
+        # 4. get a count
+        a_count = len(a)
+        # 5. compute
+        print(word_len,a_len,children_count,a_count)
+        return (word_len-a_len)/(children_count-a_count)
 
 # dt = DomTree()
-# result = dt.parse_file('tieba.html')
+# result = dt.parse_file('test.html')
+# print(result)
 # with open('tieba.json', 'w') as fw:
 #     json.dump(result,fw)
 # print(result)
