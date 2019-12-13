fca7d389d6109215d85d66ec9461f3747cbf3893
webberg <webbery@foxmail.com>
2019-12-13 18:08:30 +0800

    使用d3v4,可以正确显示title了

--- algorithm/tools/dom_tree.py
+++ algorithm/tools/dom_tree.py
@@ -8,26 +8,25 @@ class DomTree():
     def __init__(self):
         self.name_idx = 1
         # self.G = nx.Graph()
-        self.graph={}
+        self.nodes = []
+        self.links = []
         self.max_weight = 1
     
     def parse_file(self,filename):
         obj=etree.parse(filename,etree.HTMLParser())
         body=obj.xpath('/html/body/*[name(.)!="script"]')
-        return {
-            'name':'root',
-            'children':self.get_children(body,self.name_idx)
-        }
+        self.get_children(body,str(self.name_idx))
+        return {"nodes":self.nodes,"links":self.links}
 
     def parse(self,html):
-        # obj=etree.HTML(html)
-        # body=obj.xpath('/html/body/*[name(.)!="script"]')
-        # self.get_children(body,self.name_idx)
-        # return self.parse_file('test.html')
-        return self.parse_file('tieba.html')
+        obj=etree.HTML(html)
+        body=obj.xpath('/html/body/*[name(.)!="script"]')
+        self.nodes.append({"id":str(self.name_idx),"size":2.5})
+        self.get_children(body,str(self.name_idx))
+        return {"nodes":self.nodes,"links":self.links}
 
     def get_children(self,node,parent_name):
-        children = []
+        # children = []
         self.max_weight += 1
         for elm in node:
             self.name_idx += 1
@@ -37,13 +36,17 @@ class DomTree():
                 text = elm.xpath('string()')
                 # print(text)
                 # node_name += "["+ text+ "]"
-            child = {'name':node_name}
-            child['size'] = math.ceil(self.char_density(elm))
-            this_children = self.get_children(elm, node_name)
-            if len(this_children)!=0:
-                child["children"] = this_children
-            children.append(child)
-        return children
+            # child = {'name':node_name}
+            # child['size'] = math.ceil(self.char_density(elm))
+            node = {"id":node_name,"size":math.ceil(self.char_density(elm))}
+            self.nodes.append(node)
+            link = {"source":parent_name,"target":node_name}
+            self.links.append(link)
+            self.get_children(elm, node_name)
+            # if len(this_children)!=0:
+            #     child["children"] = this_children
+            # children.append(child)
+        # return children
 
     # 计算节点的字符密度
     def char_density(self,node):
