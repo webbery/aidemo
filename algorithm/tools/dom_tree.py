df817b497df0d008090bb54ccbb77a946bc970d4
webberg <webbery@foxmail.com>
2019-12-14 20:08:57 +0800

    update

--- algorithm/tools/dom_tree.py
+++ algorithm/tools/dom_tree.py
@@ -8,25 +8,37 @@ class DomTree():
     def __init__(self):
         self.name_idx = 1
         # self.G = nx.Graph()
-        self.nodes = []
-        self.links = []
+        self.graph={}
         self.max_weight = 1
+        self.second_weight = 1
+        self.size_dict={1:1}
     
     def parse_file(self,filename):
         obj=etree.parse(filename,etree.HTMLParser())
         body=obj.xpath('/html/body/*[name(.)!="script"]')
-        self.get_children(body,str(self.name_idx))
-        return {"nodes":self.nodes,"links":self.links}
+        root = obj.xpath('/html/body')
+        return {
+            'name':'root',
+            'children':self.get_children(body,self.name_idx)
+        }
+
+    def display_size(self,val):
+        return 8*math.log(val,10)
 
     def parse(self,html):
         obj=etree.HTML(html)
         body=obj.xpath('/html/body/*[name(.)!="script"]')
-        self.nodes.append({"id":str(self.name_idx),"size":2.5})
-        self.get_children(body,str(self.name_idx))
-        return {"nodes":self.nodes,"links":self.links}
+        root = obj.xpath('/html/body')
+        val = self.display_size(math.ceil(self.char_density(root[0]))+1)
+        children = self.get_children(body,self.name_idx)
+        return {
+            'name':'root',
+            'size': val,
+            'children': children
+        }
 
     def get_children(self,node,parent_name):
-        # children = []
+        children = []
         self.max_weight += 1
         for elm in node:
             self.name_idx += 1
@@ -36,28 +48,24 @@ class DomTree():
                 text = elm.xpath('string()')
                 # print(text)
                 # node_name += "["+ text+ "]"
-            # child = {'name':node_name}
-            # child['size'] = math.ceil(self.char_density(elm))
-            node = {"id":node_name,"size":math.ceil(self.char_density(elm))}
-            self.nodes.append(node)
-            link = {"source":parent_name,"target":node_name}
-            self.links.append(link)
-            self.get_children(elm, node_name)
-            # if len(this_children)!=0:
-            #     child["children"] = this_children
-            # children.append(child)
-        # return children
+            child = {'name':node_name}
+            child['size'] = self.display_size(math.ceil(self.char_density(elm))+1)
+            this_children = self.get_children(elm, node_name)
+            if len(this_children)!=0:
+                child["children"] = this_children
+            children.append(child)
+        return children
 
     # 计算节点的字符密度
     def char_density(self,node):
         # 1. get word count
         text = node.xpath('string()')
-        word_len = len(str(text))
+        word_len = len(str(text).strip())
         # 2. tag <a> word count
         a = node.xpath('.//a')
         a_text = ''
         for n in a:
-            a_text += n.xpath('string()')
+            a_text += str(n.xpath('string()')).strip()
         a_len = len(a_text)
         # 3. get all children
         children = node.xpath('//*')
@@ -65,8 +73,8 @@ class DomTree():
         # 4. get a count
         a_count = len(a)
         # 5. compute
-        print(word_len,a_len,children_count,a_count)
-        return (word_len-a_len)/(children_count-a_count)
+        # print(word_len,a_len,children_count,a_count)
+        return (word_len-a_len+1)/(children_count-a_count+1)
 
 # dt = DomTree()
 # result = dt.parse_file('test.html')
